0e41f690952aa82cd988459741d0105b78d6e73c
enrico-dgr <elcidwar@gmail.com>
2022-01-17 16:57:32 +0100

    pre-merge

--- src/components/funcComponents/InputText.js
+++ src/components/funcComponents/InputText.js
@@ -2,7 +2,7 @@ import "./inputText.css";
 
 import PropTypes from "prop-types";
 
-const Input = (props) => {
+const InputText = (props) => {
     const isError = props.errorMessage !== "";
 
     const onChange = (e) => {
@@ -11,33 +11,63 @@ const Input = (props) => {
 
     return (
         <div className={"input-text"}>
+            {props.isValid && <div className="input-text__valid-overlay"></div>}
             <input
-                type="text"
+                // autocomplete
+                name={props.name}
                 placeholder={isError ? props.errorMessage : props.placeholder}
                 className={`input-text__input ${
-                    isError ? "input-text__input--error" : ""
+                    isError
+                        ? "input-text__input--error"
+                        : props.isValid
+                        ? "input-text__input--valid"
+                        : ""
                 }`}
+                onBlur={props.onBlur}
+                onFocus={props.onFocus}
                 onChange={onChange}
+                type={props.type}
             />
-            {isError && <span className={"input-text__btn-hide-error"}>x</span>}
+            {isError && (
+                <span
+                    className={"input-text__icon input-text__icon--hide-error"}
+                >
+                    <div onClick={props.onClickXButton}>&#10005;</div>
+                </span>
+            )}
+            {props.isValid && (
+                <span
+                    className={"input-text__icon input-text__icon--valid-check"}
+                >
+                    <div>&#10003;</div>
+                </span>
+            )}
         </div>
     );
 };
 
-Input.defaultProps = {
+InputText.defaultProps = {
     errorMessage: "",
+    isValid: false,
+    name: "",
     onBlur: () => {},
     onChange: () => {},
+    onFocus: () => {},
     onClickXButton: () => {},
     placeholder: "",
+    type: "text",
 };
 
-Input.propTypes = {
+InputText.propTypes = {
     errorMessage: PropTypes.string,
+    isValid: PropTypes.bool,
+    name: PropTypes.string,
     onBlur: PropTypes.func,
     onChange: PropTypes.func,
+    onFocus: PropTypes.func,
     onClickXButton: PropTypes.func,
     placeholder: PropTypes.string,
+    type: PropTypes.string,
 };
 
-export default Input;
+export default InputText;
--- src/screens/LogIn.js
+++ src/screens/LogIn.js
@@ -2,7 +2,6 @@ import "./logIn.css";
 
 import React, { Component } from "react";
 
-import Button from "../components/funcComponents/Button";
 import Card from "../components/funcComponents/Card";
 //Components
 import InputText from "../components/funcComponents/InputText";
@@ -13,36 +12,107 @@ class LogIn extends Component {
 
         this.state = {
             username: "",
+            isUsernameValid: false,
             usernameErrorMessage: "",
+            password: "",
+            isPasswordValid: false,
+            passwordErrorMessage: "",
         };
     }
 
-    onChangeUsername = (username) => {
+    getUsernameErrorMessage = (text) => {
+        if (text === "") {
+            return "Enter username or email";
+        }
+
+        return "";
+    };
+
+    onBlurUsername = () => {
         let newState = {};
 
-        if (username === "") {
-            newState.usernameErrorMessage = "Enter username or email";
+        newState.usernameErrorMessage = this.getUsernameErrorMessage(
+            this.state.username
+        );
+
+        if (newState.usernameErrorMessage === "") {
+            newState.isUsernameValid = true;
         }
 
         this.setState(newState);
     };
 
-    onClickSignIn = () => {};
+    onChangeUsername = (username) => {
+        this.setState({ username });
+    };
+
+    onFocusUsername = () => {
+        this.setState({ isUsernameValid: false, usernameErrorMessage: "" });
+    };
+
+    onClickXButtonUsername = () => {
+        this.setState({ usernameErrorMessage: "" });
+    };
+
+    getPasswordErrorMessage = (text) => {
+        if (text === "") {
+            return "Enter password";
+        }
+
+        return "";
+    };
+
+    onBlurPassword = () => {
+        let newState = {};
+
+        newState.passwordErrorMessage = this.getPasswordErrorMessage(
+            this.state.password
+        );
+
+        if (newState.passwordErrorMessage === "") {
+            newState.isPasswordValid = true;
+        }
+
+        this.setState(newState);
+    };
+
+    onChangePassword = (password) => {
+        this.setState({ password, isPasswordValid: false });
+    };
+
+    onFocusPassword = () => {
+        this.setState({ isPasswordValid: false, passwordErrorMessage: "" });
+    };
+
+    onClickXButtonPassword = () => {
+        this.setState({ passwordErrorMessage: "" });
+    };
 
     render() {
         return (
             <Card className={"card-log-in"}>
                 <InputText
-                    placeholder="username or email"
                     errorMessage={this.state.usernameErrorMessage}
+                    isValid={this.state.isUsernameValid}
+                    name={"username"}
+                    onBlur={this.onBlurUsername}
                     onChange={this.onChangeUsername}
+                    onFocus={this.onFocusUsername}
+                    onClickXButton={this.onClickXButtonUsername}
+                    placeholder="username or email"
+                    type={"text"}
                 />
-                <Button
-                    className={"button__sign-in"}
-                    onClick={this.onClickSignIn}
-                >
-                    SIGN IN
-                </Button>
+                <InputText
+                    errorMessage={this.state.passwordErrorMessage}
+                    isValid={this.state.isPasswordValid}
+                    name={"pass"}
+                    onBlur={this.onBlurPassword}
+                    onChange={this.onChangePassword}
+                    onFocus={this.onFocusPassword}
+                    onClickXButton={this.onClickXButtonPassword}
+                    placeholder="password"
+                    type={"password"}
+                />
             </Card>
         );
     }
